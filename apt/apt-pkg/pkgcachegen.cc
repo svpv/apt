3863733eed7ba24dd7b0288ac465d99077988a82
Gleb Fotengauer-Malinovskiy <glebfm@altlinux.org>
2014-05-16 21:04:23 +0400

    Apply apt-0.5.15lorg2-lorg-cache-limit.patch
    Added in commit d5ff215:
    lorg-cache-limit.patch: increase cache size limit

--- apt/apt-pkg/pkgcachegen.cc
+++ apt/apt-pkg/pkgcachegen.cc
@@ -761,7 +761,7 @@ static bool CollectFileProvides(pkgCacheGenerator &Gen,
 bool pkgMakeStatusCache(pkgSourceList &List,OpProgress &Progress,
 			MMap **OutMap,bool AllowMem)
 {
-   unsigned long MapSize = _config->FindI("APT::Cache-Limit",12*1024*1024);
+   unsigned long MapSize = _config->FindI("APT::Cache-Limit",(32+4*sizeof(long))*1024*1024);
    
    vector<pkgIndexFile *> Files(List.begin(),List.end());
    unsigned long EndOfSource = Files.size();
@@ -974,7 +974,7 @@ bool pkgMakeStatusCache(pkgSourceList &List,OpProgress &Progress,
 /* */
 bool pkgMakeOnlyStatusCache(OpProgress &Progress,DynamicMMap **OutMap)
 {
-   unsigned long MapSize = _config->FindI("APT::Cache-Limit",8*1024*1024);
+   unsigned long MapSize = _config->FindI("APT::Cache-Limit",(32+4*sizeof(long))*1024*1024);
    vector<pkgIndexFile *> Files;
    unsigned long EndOfSource = Files.size();
    if (_system->AddStatusFiles(Files) == false)
